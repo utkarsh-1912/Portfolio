f3c745d9545b5447ced05387c902aea02ae63538
utkarsh-1912 <79002186+utkarsh-1912@users.noreply.github.com>
2022-05-20 19:42:50 +0530

    Version : 0.0.1

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+function sendMail() {
+    // Email.send({
+    //     Host: document.getElementById('email').value,
+    //     Username: document.getElementById('email').value,
+    //     Password: "Utkarsh@123",
+    //     To: 'utkrist.random.1912@gmail.com',
+    //     From: document.getElementById('email').value,
+    //     Subject: "Message from Website",
+    //     Body: document.getElementById('text').value,
+    //   })
+    //     .then(function (message) {
+    //       alert("mail sent successfully")
+    //     });
+    var link = "mailto:utkrist.random.1912@gmail.com"
+             + "?cc="+document.getElementById('email').value
+             + "&subject=" + encodeURIComponent("This is my subject")
+             + "&body=" + encodeURIComponent("Name : "+document.getElementById('name').value+"    Message"+document.getElementById('text').value)
+    ;
+    
+    window.location.href = link;
+}
